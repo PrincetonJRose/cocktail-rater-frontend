c32d3171f8807be731eabbcfaafa2165a4e7f792
PrincetonJRose <decoyzpr@gmail.com>
2019-06-10 00:35:25 -0400

    Got cocktail info to diplay on page. Need to work on styling and showing reviews.

--- src/components/CocktailInfo.js
+++ src/components/CocktailInfo.js
@@ -1,16 +1,91 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Segment } from 'semantic-ui-react'
+import { Segment, Image, Grid, GridRow, GridColumn, Menu, SegmentGroup } from 'semantic-ui-react'
 
 class CocktailInfo extends Component {
-
+    
     render() {
+        const c = this.props.cocktail
+        let ingredients = []
+        for (let i = 1; i < 16; i++) {
+            let ing = {}
+            if (c[`ingredient_${i}`] == '' || c[`ingredient_${i}`] == ' ' || c[`ingredient_${i}`] == null) {
+                break
+            }
+            ing.ingredient = c[`ingredient_${i}`]
+            if (c[`measurement_${i}`] == '' || c[`measurement_${i}`] == ' ' || c[`measurement_${i}`] == null) {
+                ing.measurement = "No measurement given."
+            } else {
+                ing.measurement = c[`measurement_${i}`]
+            }
+            ingredients.push(ing)
+        }
+        console.log(ingredients)
         return (
-            <div className="container" style={{ width: `100%`, height: `100%` }}>
-                {this.props.cocktail.name}
-            </div>
+            <Grid className="container" style={{ width: `100%`, height: `100%`, overflowY: `auto` }}>
+                <GridRow centered>
+                    <GridColumn textAlign="centered" centered><h1><u>{c.name}</u></h1></GridColumn>
+                    
+                </GridRow>
+                <GridRow stretched centered >
+                    <GridColumn width={7}>
+                        <Image className="ui medium image" src={c.imageUrl ? c.imageUrl : <span>No Image Provided</span>} style={{ borderStyle: `groove` }}/>
+                    </GridColumn>
+                    <Segment textAlign="left" inverted color="black" floated="left">
+                        <Menu fluid vertical >
+                            <Menu.Item ><b><u>Category</u>:</b> {'  ' + c.category}</Menu.Item>
+                            <Menu.Item><b><u>Alcoholic</u>?</b> {'  ' + c.alcoholic}</Menu.Item>
+                            <Menu.Item><b><u>Glass</u>:</b>{'  ' + c.glass}</Menu.Item>
+                            <Menu.Item><b><u>Video</u>?</b>{c.videoUrl ? c.videoUrl : <span>{'  '}No video provided.</span>}</Menu.Item>
+                            <Menu.Item><b><u>Likes</u>:</b>{'  ' + c.likes.length}</Menu.Item>
+                            <Menu.Item><b><u>Reviews</u>:</b>{'  ' + c.reviews.length}</Menu.Item>
+                        </Menu>
+                    </Segment>
+                </GridRow>
+                <GridRow centered>
+                    <GridColumn width={12}>
+                        <Segment inverted color="black" style={{  }}>
+                            <h3 style={{ textAlign: `center` }}><b><u>What's required</u></b></h3>
+                            <table border="1" style={{ width: `100%`, borderStyle: `dotted` }}>
+                                <tr>
+                                    <th style={{ textAlign:`center`, width:"50%" }}>Ingredient:</th>
+                                    <th style={{ textAlign:`center`, width:"50%" }}>Measurement:</th>
+                                </tr>
+                                {
+                                    ingredients.map( i => { 
+                                        return (
+                                            <tr>
+                                                <td style={{ width:"50%" }}>{i.ingredient}</td>
+                                                <td style={{ width:"50%" }}>{i.measurement}</td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </table>
+                            <h3 style={{ textAlign: `center` }}><b><u>How to create</u></b></h3>
+                            <div>{c.instructions}</div>
+                        </Segment>
+                    </GridColumn>
+                </GridRow>
+                <GridRow centered>
+                    <GridColumn width={12} >
+                        <Segment inverted color="black" >
+                        </Segment>
+                    </GridColumn>
+                </GridRow>
+            </Grid>
         )
     }
 }
+    export default connect()(CocktailInfo)
 
-export default connect()(CocktailInfo)
+    // <div className="container" style={{ width: `100%`, height: `100%`, overflowY: `auto` }}>
+    //     <h1 style={{ textAlign: `center` }}>{c.name}</h1>
+    //     <div>
+    //         <Image className="ui medium image" src={c.imageUrl ? c.imageUrl : <span>No Image Provided</span>} style={{ float: `left`, borderStyle: `groove` }}/>
+    //         <div style={{  }}>
+    //             <li><b>{c.alcoholic} bevarage</b></li>
+    //             <li><b><u>Category</u>: {c.category}</b></li>
+    //         </div>
+    //     </div>
+    // </div>
--- src/components/Cocktails.js
+++ src/components/Cocktails.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import '../style.css'
 import CocktailInfo from './CocktailInfo'
-import { Input } from 'semantic-ui-react'
+import { Input, Grid, Segment, GridColumn, GridRow } from 'semantic-ui-react'
 import { getCocktail } from '../services/APICalls'
 import { getApiCocktail } from '../services/APICalls'
 
@@ -25,7 +25,7 @@ class Cocktails extends Component {
     }
 
     filterCustomCocktails =()=> {
-        if (this.state.filter != '') {
+        if (this.state.filter !== '') {
             return this.props.custom_cocktails.filter( cocktail => {
                 if (cocktail.name.toLowerCase().includes(this.state.filter.toLowerCase()))
                 return true
@@ -58,15 +58,22 @@ class Cocktails extends Component {
                     { this.state.cocktail ?
                         <CocktailInfo cocktail={this.state.cocktail}/>
                         :
-                        <div className="container" >Hi there</div>
+                        <Grid verticalAlign="middle">
+                            <GridRow centered verticalAlign="middle">
+                                <GridColumn  width={10} verticalAlign="middle">
+                                    <Segment inverted color="black" verticalAlign="middle">
+                                    (Click one of the cocktails in the list to see it's details!)
+                                    </Segment>
+                                </GridColumn>
+                            </GridRow>
+                        </Grid>
                     }
                 </div>
                 <div className="container" style={{ width: `35%`, height: `100%`, float: `left` }}>
                     <div className="container" id="center-text" style={{ width: `100%`, height: `65%` }}>
                         <div><h3><b><u>Cocktails</u></b></h3></div>
-                        <div className="sub-content">(Click one to see it's details!)</div>
                         <Input onChange={(e)=>this.setState({ filter: e.target.value })} placeholder="Search cocktails here..."/>
-                        <div className="container" id="center-text" style={{ width: `70%`, height: `84%`, overflowX: `auto`, overflowY: `auto`, borderStyle: `groove`, textAlign: `center`, borderRadius: `12px`, display: `flex`, position: `relative`, marginLeft: `auto`, marginRight: `auto`, justifyContent: `center` }}>
+                        <div className="container" id="center-text" style={{ width: `70%`, height: `80%`, maxHeight: `84%`, overflowX: `auto`, overflowY: `auto`, borderStyle: `groove`, textAlign: `center`, borderRadius: `12px`, marginLeft: `auto`, marginRight: `auto`, marginBottom: `auto`, marginTop: `auto`, justifyContent: `center` }}>
                             <ul id="center-text">
                                 {this.filterCocktails().map( cocktail => {
                                     return <div><button onClick={()=> this.getCocktailInfo(cocktail)}>{cocktail.name}</button></div>
@@ -77,10 +84,10 @@ class Cocktails extends Component {
                     <div id="center-text" style={{ overflowX: `hidden`, overflowY: `auto`, height: `35%` }}>
                         <div><h3><b><u>Custom Drinks</u></b></h3></div>
                         <div className="sub-content">(Cocktails users have submitted)</div>
-                        <div className="container" id="center-text" style={{ width: `70%`, height: `84%`, overflowX: `auto`, overflowY: `auto`, borderStyle: `groove`, textAlign: `center`, borderRadius: `12px`, display: `flex`, position: `relative`, marginLeft: `auto`, marginRight: `auto`, justifyContent: `center` }}>
+                        <div className="container" id="center-text" style={{ width: `70%`, height: `68%`,maxHeight: `84%`, overflowX: `auto`, overflowY: `auto`, borderStyle: `groove`, textAlign: `center`, borderRadius: `12px`, display: `flex`, position: `relative`, marginLeft: `auto`, marginRight: `auto`, marginBottom: `auto`, marginTop: `auto`, justifyContent: `center` }}>
                             <ul>
                                 {this.filterCustomCocktails().map( cocktail => {
-                                    return <div><button onClick={()=> this.getCocktailInfo(cocktail)}>{cocktail.name}</button></div>
+                                    return <button onClick={()=> this.getCocktailInfo(cocktail)}>{cocktail.name}</button>
                                 })}
                             </ul>
                         </div>
